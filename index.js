6d614478f7cc5231aedd8e375ac05dd13bbeeb7f
Txus Ordorika <txusinho@gmail.com>
2017-11-28 09:10:31 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,4 @@
+//import Module from "./main";
+var Module = require("./main");
+console.log(Object.keys(Module));
+console.log(Module._int_sqrt(9)); //eslint-disable-line no-console
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <math.h>
+
+extern "C" {
+    int int_sqrt(int x) {
+        return sqrt(x);
+    }
+}
